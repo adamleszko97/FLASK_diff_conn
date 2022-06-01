0a71634c2610910270ebaa12f5d8948d1eb801c5
adamleszko97 <aleszko97@gmail.com>
2022-06-01 14:48:03 +0200

    v2

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asdf'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ale@mysqlserverale:Test12345!@mysqlserverale.mysql.database.azure.com/test'
 
     from .views import views
     from .auth import auth
