73d1f056bcf766726f831fb1c3bdee11542f959c
adamleszko97 <aleszko97@gmail.com>
2022-06-03 15:09:31 +0200

    search

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import employees
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -10,5 +12,23 @@ def home():
 @views.route('/search', methods=['GET', 'POST'])
 @login_required
 def search():
+
+
+    if request.method == 'POST':
+        name_form = request.form.get('name_search')
+
+        if(len(name_form) > 2):
+            result = employees.query.filter(employees.name.contains(name_form)).order_by(employees.id).all()
+            
+            if result:
+                flash('Users found!')
+
+            else:
+                flash('Users not found', category='error')  
+
+        else:
+            flash('Please provide more than 2 characters', category='error')
+        
+        
     return render_template("search.html", user=current_user)
 
